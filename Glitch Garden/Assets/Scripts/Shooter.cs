395764126dd0b21e02348bd073a274b5f20eafe2
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-03 12:44:29 +0100

    152
    Attacker in my lane

--- Glitch Garden/Assets/Scripts/Shooter.cs
+++ Glitch Garden/Assets/Scripts/Shooter.cs
@@ -6,10 +6,55 @@ public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile;
     [SerializeField] GameObject gun;
+    AttackerSpawner myLaneSpawner;
 
+    private void Start()
+    {
+        SetLaneSpawner();
+    }
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            Debug.Log("shoot");
+        }
+        else
+        {
+            Debug.Log("Chill");
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            bool IsCloseEnough = 
+                (Mathf.Abs(spawner.transform.position.y - transform.position.y)
+                <=Mathf.Epsilon);
+            if(IsCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
     public void Fire()
     {
         Instantiate(projectile, gun.transform.position, Quaternion.identity);
     }
 
+    
 }
