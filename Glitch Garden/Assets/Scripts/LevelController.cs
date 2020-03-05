1b53af66fa784b04aa8dc2093fa525a085c88e26
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-05 12:00:49 +0100

    164
    WinExperience

--- Glitch Garden/Assets/Scripts/LevelController.cs
+++ Glitch Garden/Assets/Scripts/LevelController.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
+    [SerializeField] float waitToLoad = 4f;
+    [SerializeField] GameObject winLabel;
     int numberOfAttackers = 0;
     bool levelTimerFinished = false;
+    
 
+    private void Start()
+    {
+        winLabel.SetActive(false);
+    }
 
     public void AttackerSpawned()
     {
@@ -18,8 +25,16 @@ public class LevelController : MonoBehaviour
         numberOfAttackers--;
         if(numberOfAttackers <= 0 && levelTimerFinished)
         {
-            Debug.Log("end lvl");
+            StartCoroutine(HandleWinCondition());
         }
+    }
+
+    IEnumerator HandleWinCondition()
+    {
+        winLabel.SetActive(true);
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(waitToLoad);
+        FindObjectOfType<LevelLoader>().LoadNextScene();
     }
 
     public void LevelTimerFinished()
