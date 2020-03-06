251457c6c684f73c4565007b635904da124ed83a
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-03-06 16:58:14 +0100

    173

--- Glitch Garden/Assets/Scripts/Shooter.cs
+++ Glitch Garden/Assets/Scripts/Shooter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,12 +9,26 @@ public class Shooter : MonoBehaviour
     [SerializeField] GameObject gun;
     AttackerSpawner myLaneSpawner;
     Animator animator;
+    GameObject projectileParent;
+    const string PROJECTILE_PARENT_NAME = "Projectiles";
+
 
     private void Start()
     {
         SetLaneSpawner();
         animator = GetComponent<Animator>();
+        CreateProjectileParent();
     }
+
+    private void CreateProjectileParent()
+    {
+        projectileParent = GameObject.Find(PROJECTILE_PARENT_NAME);
+        if(!projectileParent)
+        {
+            projectileParent = new GameObject(PROJECTILE_PARENT_NAME);
+        }
+    }
+
     private void Update()
     {
         if (IsAttackerInLane())
@@ -55,7 +70,10 @@ public class Shooter : MonoBehaviour
     }
     public void Fire()
     {
-        Instantiate(projectile, gun.transform.position, Quaternion.identity);
+        GameObject newProjectile = 
+            Instantiate(projectile, gun.transform.position, Quaternion.identity)
+            as GameObject;
+        newProjectile.transform.parent = projectileParent.transform;
     }
 
     
